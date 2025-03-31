1974c2ee9c73ab15f5c7511fe835892b51a48abf
30003000666 <30003000666@brilife.co.id>
2025-04-01 00:40:19 +0700

    pastoral

--- src/components/Pastoral.js
+++ src/components/Pastoral.js
@@ -14,21 +14,58 @@ function Pastoral() {
   const navigate = useNavigate();
 
   const fetchDataBaptism = async () => {
-    try {
-      const response = await fetch(`https://api.gppkcbn.org/cbn/v1/service/baptisan/getFormBaptisan`);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      console.log('=================fff===================');
-      console.log(data);
-      console.log('====================================');
-      return data.data;
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      throw error;
-    }
+    const response = await fetch(`https://api.gppkcbn.org/cbn/v1/service/baptisan/getFormBaptisan`);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const data = await response.json();
+    return data.data;
   };
+
+  // Fetch data untuk Konseling
+  const fetchDataKonseling = async () => {
+    const response = await fetch(`https://api.gppkcbn.org/cbn/v1/service/konseling/getData`);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const data = await response.json();
+    return data.data;
+  };
+  // const fetchDataKartuJemaat = async () => {
+  //   const response = await fetch(`https://api.gppkcbn.org/cbn/v1/service/kartuJemaat/getData`);
+  //   if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+  //   const data = await response.json();
+  //   return data.data;
+  // };
+
+  // Fetch data untuk Pendaftaran Pelayan (contoh, sesuaikan dengan endpoint Anda)
+  const fetchDataPelayan = async () => {
+    const response = await fetch(`https://api.gppkcbn.org/cbn/v1/service/pelayan/getData`);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const data = await response.json();
+    return data.data;
+  };
+
+  // Fetch data untuk Pemberkatan Pernikahan (contoh, sesuaikan dengan endpoint Anda)
+  const fetchDataPernikahan = async () => {
+    const response = await fetch(`https://api.gppkcbn.org/cbn/v1/service/pernikahan/getData`);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const data = await response.json();
+    return data.data;
+  };
+
+  // Fetch data untuk Pemberkatan Rumah (contoh, sesuaikan dengan endpoint Anda)
+  const fetchDataRumah = async () => {
+    const response = await fetch(`https://api.gppkcbn.org/cbn/v1/service/pemberkatanRumah/getData`);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const data = await response.json();
+    return data.data;
+  };
+
+  // Fetch data untuk Penyerahan Anak (contoh, sesuaikan dengan endpoint Anda)
+  const fetchDataAnak = async () => {
+    const response = await fetch(`https://api.gppkcbn.org/cbn/v1/service/penyerahanAnak/getData`);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const data = await response.json();
+    return data.data;
+  };
+
   const deleteBaptism = async (id) => {
     try {
       const response = await fetch(`https://api.gppkcbn.org/cbn/v1/service/baptisan/deleteFormBaptisan`, {
@@ -50,17 +87,10 @@ function Pastoral() {
 
 
   const columnsBaptism = [
-
-    { key: 'TanggalPendaftaran', label: 'Tanggal Daftar',  render: (item) => {
-      return moment(item.TanggalLahir).format('DD-MM-YYYY');
-    },
-  },
+    { key: 'TanggalPendaftaran', label: 'Tanggal Daftar', render: (item) => moment(item.TanggalLahir).format('DD-MM-YYYY') },
     { key: 'NamaLengkap', label: 'Nama' },
     { key: 'TempatLahir', label: 'Tempat Lahir' },
-    { key: 'TanggalLahir', label: 'Tgl Lahir',  render: (item) => {
-      return moment(item.TanggalLahir).format('DD-MM-YYYY');
-    },
-  },
+    { key: 'TanggalLahir', label: 'Tgl Lahir', render: (item) => moment(item.TanggalLahir).format('DD-MM-YYYY') },
     { key: 'NoHP', label: 'No. HP' },
     { key: 'KepercayaanLama', label: 'Kepercayaan Lama' },
     {
@@ -75,11 +105,137 @@ function Pastoral() {
       ),
     },
   ];
+  const columnsKonseling = [
+    { key: 'dateInsert', label: 'Tgl', render: (item) => moment(item.dateInsert).format('DD-MMM'), maxWidth: 60 },
+    { key: 'jenisKonsultasi', label: 'Konsul', maxWidth: 80 },
+    { key: 'fullName', label: 'Nama', flexGrow: 1, style: { textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' } },
+    { key: 'noHP', label: 'No. HP', maxWidth: 120 },
+    { key: 'sex', label: 'J. Kelamin', maxWidth: 100 },
+    { key: 'statusPernikahan', label: 'Status', maxWidth: 100 },
+    { key: 'TanggalLahir', label: 'Tgl Lahir', render: (item) => moment(item.TanggalLahir).format('DD-MM-YYYY'), maxWidth: 100 },
+    { key: 'alamat', label: 'Alamat', flexGrow: 2, style: { textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' } },
+    { key: 'isi', label: 'Detail', flexGrow: 2, style: { textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' } },
+    {
+      key: 'actions',
+      label: 'Aksi',
+      maxWidth: 150,
+      render: (item) => (
+        <div className="actions">
+          <button className="btn btn-delete" onClick={() => handleDelete(item.id)}>Hapus</button>
+          <button className="btn btn-secondary">Edit</button>
+          <button className="btn btn-detail" onClick={() => navigate(`/dashboard/detail-konseling/${item.id}`)}>Detail</button>
+        </div>
+      ),
+    },
+  ];
+  const columnsPelayan = [
+   { key: 'namaLengkap', label: 'Nama Lengkap'},
+   { key: 'nomorHandphone', label: 'Nomor Handphone'},
+   { key: 'bidangPelayanan', label: 'Bidang Pelayanan'},
 
-  const { data, isLoading, error } = useQuery({
-    queryKey: ['dataBaptism'],
-    queryFn: fetchDataBaptism,
-  });
+    {
+      key: 'actions',
+      label: 'Aksi',
+      render: (item) => (
+        <div className="actions">
+          <button className="btn btn-delete" onClick={() => handleDelete(item.id)}>Hapus</button>
+          <button className="btn btn-secondary">Edit</button>
+        </div>
+      ),
+    },
+  ];
+  const columnsPernikahan = [
+    { key: 'tanggal_pernikahan', label: 'Tgl Pernikahan',render: (item) => moment(item.tanggal_pernikahan).format('DD-MM-YYYY')},
+    { key: 'nama_lengkap_pria', label: 'Nama Lengkap Pria'},
+    { key: 'telepon_pria', label: 'Nomor Handphone Pria'},
+    { key: 'nama_lengkap_wanita', label: 'Nama Lengkap Wanita'},
+    { key: 'telepon_wanita', label: 'Nomor Handphone Wanita'},
+ 
+     {
+       key: 'actions',
+       label: 'Aksi',
+       maxWidth: 150,
+       render: (item) => (
+         <div className="actions">
+           <button className="btn btn-delete" onClick={() => handleDelete(item.id)}>Hapus</button>
+           <button className="btn btn-secondary">Edit</button>
+           <button className="btn btn-detail" onClick={() => navigate(`/dashboard/detail-konseling/${item.id}`)}>Detail</button>
+
+         </div>
+       ),
+     },
+   ];
+   const columnsRumah = [
+    // { key: 'created_at', label: 'Date',render: (item) => moment(item.created_at).format('DD-MM-YYYY')},
+    { key: 'nama_lengkap', label: 'Nama Lengkap'},
+    { key: 'nomor_handphone', label: 'Nomor Handphone'},
+    { key: 'tanggal_pelaksanaan', label: 'Tanggal Pelaksanaan',render: (item) => moment(item.tanggal_pelaksanaan).format('DD-MM-YYYY')},
+ 
+     {
+       key: 'actions',
+       label: 'Aksi',
+       maxWidth: 150,
+       render: (item) => (
+         <div className="actions">
+           <button className="btn btn-delete" onClick={() => handleDelete(item.id)}>Hapus</button>
+              </div>
+       ),
+     },
+   ];
+   const columnsAnak = [
+    // { key: 'created_at', label: 'Date',render: (item) => moment(item.created_at).format('DD-MM-YYYY')},
+    { key: 'nama_ayah', label: 'Nama Lengkap Ayah'},
+    { key: 'telepon_ayah', label: 'NO HP Ayah'},
+    {key: 'nama_ibu', label: 'Nama Lengkap Ibu'},
+    { key: 'telepon_ibu', label: 'NO HP Ibu'},
+    {key: 'nama_anak', label: 'Nama Anak'},
+    { key: 'tempat_tanggal_lahir_anak', label: 'TTL Anak'},
+    { key: 'tanggal_pelaksanaan', label: 'Tanggal Pelaksanaan',render: (item) => moment(item.tanggal_pelaksanaan).format('DD-MM-YYYY')},
+ 
+    {
+      key: 'actions',
+      label: 'Aksi',
+      maxWidth: 150,
+      render: (item) => (
+        <div className="actions">
+          <button className="btn btn-delete" onClick={() => handleDelete(item.id)}>Hapus</button>
+          <button className="btn btn-secondary">Edit</button>
+          <button className="btn btn-detail" onClick={() => navigate(`/dashboard/detail-konseling/${item.id}`)}>Detail</button>
+
+        </div>
+      ),
+    },
+   ]; 
+  //   { key: 'dateInsert', label: 'Tgl', render: (item) => moment(item.dateInsert).format('DD-MMM'), maxWidth: 60 },
+  //   { key: 'jenisKonsultasi', label: 'Konsul', maxWidth: 80 },
+  //   { key: 'fullName', label: 'Nama', flexGrow: 1, style: { textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' } },
+  //   { key: 'noHP', label: 'No. HP', maxWidth: 120 },
+  //   { key: 'sex', label: 'J. Kelamin', maxWidth: 100 },
+  //   { key: 'statusPernikahan', label: 'Status', maxWidth: 100 },
+  //   { key: 'TanggalLahir', label: 'Tgl Lahir', render: (item) => moment(item.TanggalLahir).format('DD-MM-YYYY'), maxWidth: 100 },
+  //   { key: 'alamat', label: 'Alamat', flexGrow: 2, style: { textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' } },
+  //   { key: 'isi', label: 'Detail', flexGrow: 2, style: { textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' } },
+  //   {
+  //     key: 'actions',
+  //     label: 'Aksi',
+  //     maxWidth: 150,
+  //     render: (item) => (
+  //       <div className="actions">
+  //         <button className="btn btn-delete" onClick={() => handleDelete(item.id)}>Hapus</button>
+  //         <button className="btn btn-secondary">Edit</button>
+  //         <button className="btn btn-detail" onClick={() => navigate(`/dashboard/detail-konseling/${item.id}`)}>Detail</button>
+  //       </div>
+  //     ),
+  //   },
+  // ];
+  const { data: dataBaptism, isLoading: loadingBaptism, error: errorBaptism } = useQuery({ queryKey: ['dataBaptism'], queryFn: fetchDataBaptism });
+  const { data: dataKonseling, isLoading: loadingKonseling, error: errorKonseling } = useQuery({ queryKey: ['dataKonseling'], queryFn: fetchDataKonseling });
+  // const { data: dataKartuJemaat, isLoading: loadingKartuJemaat, error: errorKartuJemaat } = useQuery({ queryKey: ['dataKartuJemaat'], queryFn: fetchDataKartuJemaat });
+  const { data: dataPelayan, isLoading: loadingPelayan, error: errorPelayan } = useQuery({ queryKey: ['dataPelayan'], queryFn: fetchDataPelayan });
+  const { data: dataPernikahan, isLoading: loadingPernikahan, error: errorPernikahan } = useQuery({ queryKey: ['dataPernikahan'], queryFn: fetchDataPernikahan });
+  const { data: dataRumah, isLoading: loadingRumah, error: errorRumah } = useQuery({ queryKey: ['dataRumah'], queryFn: fetchDataRumah });
+  const { data: dataAnak, isLoading: loadingAnak, error: errorAnak } = useQuery({ queryKey: ['dataAnak'], queryFn: fetchDataAnak });
+
 
   const deleteMutation = useMutation({
     mutationFn: deleteBaptism,
@@ -118,40 +274,34 @@ function Pastoral() {
 
   return (
     <div>
-      <div className="mb-10">
+   <div className="mb-10">
         <h2>Permohonan Baptisan</h2>
-        {isLoading ? (
-          <CircularProgress />
-        ) : error ? (
-          <Typography color="error">There was an error loading the data.</Typography>
-        ) : (
-          <DynamicTable columns={columnsBaptism} data={data} />
-        )}
+        {loadingBaptism ? <CircularProgress /> : errorBaptism ? <Typography color="error">Error loading data.</Typography> : <DynamicTable columns={columnsBaptism} data={dataBaptism} />}
       </div>
-      <div>
+      <div className="mb-10">
         <h2>Permohonan Konseling</h2>
-        {/* Konten data service */}
+        {loadingKonseling ? <CircularProgress /> : errorKonseling ? <Typography color="error">Error loading data.</Typography> : <DynamicTable columns={columnsKonseling} data={dataKonseling} />}
       </div>
-      <div>
+     {/* <div className="mb-10">
         <h2>Permohonan Kartu Jemaat</h2>
-        {/* Konten data service */}
-      </div>
-      <div>
+        {loadingKartuJemaat ? <CircularProgress /> : errorKartuJemaat ? <Typography color="error">Error loading data.</Typography> : <DynamicTable columns={columnsKartu} data={dataKartuJemaat} />}
+      </div> */}
+      <div className="mb-10">
         <h2>Pendaftaran Pelayan</h2>
-        {/* Konten data service */}
-      </div>
-      <div>
+        {loadingPelayan ? <CircularProgress /> : errorPelayan ? <Typography color="error">Error loading data.</Typography> : <DynamicTable columns={columnsPelayan} data={dataPelayan} />}
+      </div> 
+      <div className="mb-10">
         <h2>Pemberkatan Pernikahan</h2>
-        {/* Konten data service */}
+        {loadingPernikahan ? <CircularProgress /> : errorPernikahan ? <Typography color="error">Error loading data.</Typography> : <DynamicTable columns={columnsPernikahan} data={dataPernikahan} />}
       </div>
-      <div>
+       <div className="mb-10">
         <h2>Pemberkatan Rumah</h2>
-        {/* Konten data service */}
+        {loadingRumah ? <CircularProgress /> : errorRumah ? <Typography color="error">Error loading data.</Typography> : <DynamicTable columns={columnsRumah} data={dataRumah} />}
       </div>
-      <div>
+       <div className="mb-10">
         <h2>Penyerahan Anak</h2>
-        {/* Konten data service */}
-      </div>
+        {loadingAnak ? <CircularProgress /> : errorAnak ? <Typography color="error">Error loading data.</Typography> : <DynamicTable columns={columnsAnak} data={dataAnak} />}
+      </div>  
     </div>
   );
 }
